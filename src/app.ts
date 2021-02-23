f68279a3b473d47b1af7b94ec5908e618cd2c387
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-02-23 12:23:38 -0300

    feat: controller and User model

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
 
+import router from "./routes";
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(router);
 
 export default app;
