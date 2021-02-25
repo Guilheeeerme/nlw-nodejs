bb6b33636466c6e9139c76f660a5d98aeb07d885
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-02-25 19:27:22 -0300

    feat: sending email

---
+++ src/services/SendMailService.ts
@@ -0,0 +1,43 @@
+import * as nodemailer from "nodemailer";
+import * as handlebars from "handlebars";
+import * as fs from "fs";
+
+class SendMailService {
+  private client: nodemailer.Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then((account) => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString("utf8");
+
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html,
+      from: "NPS <noreplay@nps.com.br>",
+    });
+
+    console.log("Message sent %s", message.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
+  }
+}
+
+export default new SendMailService();
